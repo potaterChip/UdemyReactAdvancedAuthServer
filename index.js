23349c620dc244f8fe2c0a5e2a44bf96135cbce2
Eric <ericgrobin@gmail.com>
2018-07-07 13:12:08 -0500

    Adding cors configuration.

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const morgan = require('morgan')
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose')
+const cors = require('cors');
 
 mongoose.connect('mongodb://localhost/auth');
 
 // app setup
-app.use(morgan('combined'))
+app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
